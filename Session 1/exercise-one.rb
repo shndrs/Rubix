bf09a342576f5650c88270a2c2b07a821e089056
NP2 <sahandraeisi1994@gmail.com>
2019-08-21 14:23:51 +0430

    Added exercise

---
+++ Session 1/exercise-one.rb
@@ -0,0 +1,26 @@
+# ex 1 - session 1
+
+print("Who is your favorite Formula 1 driver? ")
+
+name = gets
+name = name.chomp
+
+print("Name his team? ")
+
+team = gets
+team = team.chomp
+
+print("his teammate : ")
+
+teammate = gets
+teammate = teammate.chomp
+
+puts
+
+puts(name + ", is your favorite driver")
+puts
+puts("he drives for " + team + ".")
+puts
+puts("And his teammate is : " + teammate + ".")
+puts
+
