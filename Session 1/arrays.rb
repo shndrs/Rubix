6c42a4100f3bf6c3b71f320918c6ed95b6467277
NP2 <sahandraeisi1994@gmail.com>
2019-08-21 12:41:17 +0430

    Added arrays examples and gave it execution permission

---
+++ Session 1/arrays.rb
@@ -0,0 +1,12 @@
+array1 = [1,2,3,4,5,6]
+
+print(array1, "\n")
+
+array2 = ["shndrs","sbvttl","chrlslclrc","frrr"]
+print(array2, "\n")
+
+print("array2[0] = ", array2[0], "\n")
+print("array2[8] = ", array2[8], "\n")
+
+digits = 0..9
+print(digits.to_a)
