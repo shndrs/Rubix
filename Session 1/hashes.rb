8595e99e5a68a35b5e717e13ab8de57df5fc4ce8
NP2 <sahandraeisi1994@gmail.com>
2019-08-21 13:42:03 +0430

    Added dictionary example to hashes.rb

---
+++ Session 1/hashes.rb
@@ -0,0 +1,12 @@
+# simple example of hash is Dictionary
+
+numbers = {
+    'Vettel' => '5',
+    'Leclerc' => '16',
+    'Albon' => '20',
+    'Raikonnen' => '7'
+}
+
+print(numbers, "\n")
+print("Vettel car Number is = ", numbers['Vettel'], "\n")
+print("Vettel car Number is = ", numbers['Hamilton'], "\n") # return nil
