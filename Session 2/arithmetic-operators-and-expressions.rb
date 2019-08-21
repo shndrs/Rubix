fc3555d7ecb00d274d336af725917ea1401bb892
NP2 <sahandraeisi1994@gmail.com>
2019-08-21 15:35:11 +0430

    Added example to arithmetic-operators-and-expressions.rb

---
+++ Session 2/arithmetic-operators-and-expressions.rb
@@ -0,0 +1,23 @@
+add = 665 + 1
+puts(add)
+
+sub = -12.3 + 3.5
+puts(sub)
+
+mul = 12 * 3
+puts(mul)
+
+div = 36 / 3
+puts(div)
+
+mod = 13 % 5
+puts(mod)
+
+power = 4 ** 4
+puts(mod)
+
+order1 = 2 + 3 * 4
+puts(order1)
+
+order2 = 2 + ((3 ** 3) - 7) * 2
+puts(order2)
