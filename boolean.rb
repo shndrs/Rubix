abc1be151a97fd8c700ff1f868c14d9bae31fb8e
NP2 <sahandraeisi1994@gmail.com>
2019-08-21 11:53:07 +0430

    Added exampel of how booleans works in Ruby

---
+++ boolean.rb
@@ -0,0 +1,10 @@
+bool1 = true
+bool2 = false
+
+print("bool1 = ", bool1,", bool2 = ", bool2 , "\n")
+print("!bool1 = ",!bool1, "\n")
+print("!!bool1 = ", !!bool1 , "\n")
+
+print("1 > 10 = ", 1 > 10 , "\n")
+print("1 < 10 = ", 1 < 10 , "\n")
+
