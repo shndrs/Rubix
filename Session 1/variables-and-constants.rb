b5b07258a9ccf9a05d97caed2cbdceb02979aec7
NP2 <sahandraeisi1994@gmail.com>
2019-08-21 13:57:22 +0430

    Added example to variables-and-constants.rb

---
+++ Session 1/variables-and-constants.rb
@@ -0,0 +1,22 @@
+# local variables and constant
+# ruby use '_' for define stuffs like phone_number
+
+fullname = "sahand raeisi"
+
+# Global variable creating by putting '$' behind of variable name
+
+$token = "iefe49940jejkej332ss02s2dasds32112dssdsds-d441qazslkd"
+
+puts($token)
+
+# Constants --> Creating by Capital letters
+
+PI = 3.14159
+T = -273
+
+puts(PI, T)
+
+PI = 3
+T = 8
+
+# gave us warning
