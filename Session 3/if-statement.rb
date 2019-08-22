a198a1fdc4065b0bb6000ca73f848557a19871fb
Sahand Raeisi <sahandraeisi@yahoo.com>
2019-08-22 12:05:09 +0430

    Added to if-statement.rb an example

---
+++ Session 3/if-statement.rb
@@ -0,0 +1,10 @@
+# if comparison (relational expression)
+#     statement
+
+puts("Please enter your grade : ")
+grade = gets
+grade = Integer(grade)
+
+if (grade >= 12) && (grade <= 20)
+    puts("passed")
+end
