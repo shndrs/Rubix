a1fd5f2780980cd9273d25cba27c6916d9f7cce9
Sahand Raeisi <sahandraeisi@yahoo.com>
2019-08-22 11:52:51 +0430

    Added some code to exersice-one.rb

---
+++ Session 2/exersice-one.rb
@@ -0,0 +1,9 @@
+x = -11
+y = 3.1
+
+print(" x = ",x, ", y = ", y, "\n")
+puts("===========================")
+print(" x / y-3 = ", x / y-3, "\n")
+print(" 1 / (x+y) = ", 1 / (x+y), "\n")
+print(" Math.sqrt(x**6+y**5) = ", Math.sqrt(x**6+y**5), "\n")
+print(" (x+y).abs = ", (x+y).abs, "\n")
