1cec14cca0a08493bf1ea13e70b81fcd64eafa29
NP2 <sahandraeisi1994@gmail.com>
2019-08-24 14:03:05 +0430

    Added for-in example to the for-in-for-each-loops.rb

---
+++ Session 4/for-in-for-each-loops.rb
@@ -0,0 +1,16 @@
+nums = [1,2,3,4,5,6,7,8,9, 10]
+
+for num in nums
+    print num, "\n"
+end
+
+puts("===========================")
+
+sum = 0
+
+for num in nums
+    sum += num
+end
+print sum, "\n"
+
+puts("===========================")
