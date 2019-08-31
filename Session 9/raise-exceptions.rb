c35302be3c0a478364138e28d790dee2bdfd5867
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-08-31 14:19:52 +0430

    Updated raise-exceptions.rb with an exception

--- Session 9/raise-exceptions.rb
+++ Session 9/raise-exceptions.rb
@@ -1,4 +1,17 @@
 
-print("In program...")
-raise "exeption raised" # code after this raised never execute
-print("Back in program.")
+# print("In program...")
+# raise "exeption raised " # code after this raised never execute
+# print("Back in program.")
+
+def ctof(temp)
+    raise ArgumentError, "Argument is not numeric" unless temp.is_a? Numeric
+    return (9.0/5.0) * temp + 32.0
+end
+
+begin
+    print("Enter a temprature to convert: ")
+    t = Integer(gets)
+    print(ctof(t), "\n")
+rescue
+    print("Argument was't valid\n")
+end
